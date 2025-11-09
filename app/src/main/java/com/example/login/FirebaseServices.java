995c6d6c4df529aadb5aec13479c47ea10bee1e3
rawah <rawahalimosa786@gmail.com>
2025-11-09 17:24:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/login/FirebaseServices.java
@@ -0,0 +1,42 @@
+package com.example.login;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+
+public class FirebaseServices {
+    private static FirebaseServices instance;
+    private FirebaseAuth auth;
+    private FirebaseFirestore firestore;
+    private FirebaseStorage storage;
+
+    public FirebaseServices(){
+        auth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+
+    }
+
+    public FirebaseAuth getAuth() {
+        return auth;
+    }
+
+    public FirebaseStorage getStorage() {
+        return storage;
+    }
+
+
+    public FirebaseFirestore getFirestore() {
+        return firestore;
+    }
+
+
+    public static FirebaseServices getInstance(){
+        if (instance == null){
+            instance = new FirebaseServices();
+        }
+        return instance;
+    }
+
+}
+
